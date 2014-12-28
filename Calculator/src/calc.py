006e24307a930a81a26135d805b98c92406ad080
developer <developer@localhost.localdomain>
2014-12-28 18:07:15 +0300

    Loads expr from the file

--- Calculator/src/calc.py
+++ Calculator/src/calc.py
@@ -1,19 +1,37 @@
 __author__ = 'yuri'
 from Lib.InToPostNotation import InToPostNotation
 from Lib.CalculateRPN import CalculateRPN
+import sys
 
-print "Expression for calculation ( ex: (5+5)*5/sin(.5)+sqrt(25) ):"
-while True:
-    expression = raw_input("Enter your expression: ")
-    # Exit action
-    if expression == 'exit':
-        break
+
+def calculate(string):
     try:
-        rpn = InToPostNotation().get_rpn(expression)
-        result = "Result = " + str(CalculateRPN().calculate_expr(rpn))
+        rpn = InToPostNotation().get_rpn(string)
+        result = string + " = " + str(CalculateRPN().calculate_expr(rpn))
     except IndexError:
         result = 'Syntax Error: invalid syntax'
     except Exception as e:
         result = e
     finally:
         print result
+
+if len(sys.argv) > 1:
+    """If the file of expressions"""
+    try:
+        fe = open(sys.argv[1])
+        for expr in fe.read().splitlines():
+            calculate(expr)
+    except IOError as io:
+        print io
+else:
+    print "Expression for calculation ( ex: (5+5)*5/sin(.5)+sqrt(25) ):"
+    while True:
+        try:
+            expression = raw_input("Enter your expression: ")
+            # Exit action
+            if expression in ('exit', 'quit'):
+                break
+            else:
+                calculate(expression)
+        except KeyboardInterrupt:
+            exit()
